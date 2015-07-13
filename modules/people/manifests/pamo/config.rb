1e9a66aa8e97ad9bc5c95a773018fd2382802a3e
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-07-13 07:37:10 +0200

    Sets additional OS X defaults

--- modules/people/manifests/pamo/config.rb
+++ modules/people/manifests/pamo/config.rb
@@ -3,15 +3,33 @@ class people::pamo::config (
   $my_sourcedir = $people::pamo::params::my_sourcedir,
   $my_username  = $people::pamo::params::my_username
 ) {
-  include osx::dock::autohide
+
+  notify { 'Setting OS X defaults': }
   include osx::no_network_dsstores
-  include osx::software_update
-  include osx::global::key_repeat_rate
   include osx::disable_app_quarantine
-  include osx::global::disable_autocorrect
+  include osx::software_update
+
+  # global settings
+  include osx::global::key_repeat_rate
+  include osx::global::expand_print_dialog
   include osx::global::expand_save_dialog
   include osx::global::tap_to_click
+  include osx::global::disable_autocorrect
+
+  # dock
+  include osx::dock::autohide
+  include osx::dock::disable_dashboard
+
+  # finder
   include osx::finder::unhide_library
   include osx::finder::show_all_on_desktop
   include osx::finder::enable_quicklook_text_selection
+  include osx::finder::show_all_filename_extensions
+  include osx::finder::show_hidden_files
+
+  # hot corners
+  class { 'osx::dock::hot_corners':
+          top_right => "Desktop",
+          bottom_left => "Application Windows"
+  }
 }
