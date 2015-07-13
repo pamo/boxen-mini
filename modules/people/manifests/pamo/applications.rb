488c0266c613d824d8dd0969809cc7a4b489e8e3
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-07-14 01:20:57 +0200

    Installs casks one by one

--- modules/people/manifests/pamo/applications.rb
+++ modules/people/manifests/pamo/applications.rb
@@ -9,25 +9,22 @@ class people::pamo::applications {
     'libxml2',
   ]
 
-  $cask_packages = [
-    '1password',
-    'alfred',
-    'dropbox',
-    'google-chrome',
-    'google-hangouts',
-    'google-notifier',
-    'flux',
-    'flash-player',
-    'libreoffice',
-    'screenhero',
-    'spotify',
-    'skype',
-    'the-unarchiver',
-    'transmission',
-    'vlc'
-  ]
-
-  ## Declare all Homebrew packages at once
   package { $homebrew_packages }
-  package { $cask_packages: provider => 'brewcask', install_options => '--appdir=/Applications' }
+
+  package { '1password' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'alfred' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'dropbox' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'google-chrome' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'google-hangouts' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'google-notifier' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'flux' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'flash-player' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'libreoffice' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'screenhero' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'spotify' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'skype' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'the-unarchiver' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'transmission' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+  package { 'vlc' : provider => 'brewcask', install_options => '--appdir=/Applications' }
+
 }
