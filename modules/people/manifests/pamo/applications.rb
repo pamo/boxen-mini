0b656b32914ac0c6557205cea7178b90d89a74ed
Pamela Ocampo <pamela.ocampo@gmail.com>
2015-07-14 01:53:31 +0200

    Removes homebrew packages

--- modules/people/manifests/pamo/applications.rb
+++ modules/people/manifests/pamo/applications.rb
@@ -1,30 +1,20 @@
 class people::pamo::lications {
   include brewcask
   include popcorntime
-
-  $homebrew_packages = [
-    'tree',
-    'curl',
-    'ag',
-    'libxml2',
-  ]
-
-  package { $homebrew_packages }
-
-  package { '1password' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'alfred' : provider => 'brewcask', install_options =>  ['--appdir=/Applications'] }
-  package { 'dropbox' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'google-chrome' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'google-hangouts' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'google-notifier' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'flux' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'flash-player' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'libreoffice' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'screenhero' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'spotify' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'skype' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'the-unarchiver' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'transmission' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
-  package { 'vlc' : provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { '1password': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'alfred': provider => 'brewcask', install_options =>  ['--appdir=/Applications'] }
+  package { 'dropbox': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'google-chrome': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'google-hangouts': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'google-notifier': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'flux': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'flash-player': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'libreoffice': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'screenhero': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'spotify': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'skype': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'the-unarchiver': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'transmission': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
+  package { 'vlc': provider => 'brewcask', install_options => ['--appdir=/Applications'] }
 
 }
